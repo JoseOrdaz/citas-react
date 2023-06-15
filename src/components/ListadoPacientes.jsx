c895536fa0ccd00b246fba29846a308425a4b47f
JoseOrdaz <joseordazsuay@gmail.com>
2023-06-15 23:29:53 +0200

    añadir componente pacientes

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -1,9 +1,22 @@
+import Paciente from "./Paciente"
 const ListadoPacientes = () => {
   return (
-    <div className="md:w-1/2 lg:w-3/5">
+    <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll">
 
-    <h2>ListadoPacientes</h2>
+    <h2 className="font-black text-center text-3xl">ListadoPacientes</h2>
+    <p className="text-xl mt-5 mb-10 text-center">
+        Administra tus {" "}
+        <span className="text-sky-700 font-bold">Pacientes y citas</span>
+    </p>
+    <Paciente />
+    <Paciente />
+    <Paciente />
+    <Paciente />
+    <Paciente />
+
+
     </div>
+    
   )
 }
 
---
+++ src/components/Paciente.jsx
@@ -0,0 +1,24 @@
+const Paciente = () => {
+  return (
+    <div className="mt-10 bg-white shadow-md m-3 px-5 py-10 rounded-lg">
+    <p className="font-bold mb-3 text-gray-700 uppercase">Nombre:{" "}
+         <span className=" font-normal normal-case">Hook</span>
+     </p> 
+     <p className="font-bold mb-3 text-gray-700 uppercase">Propietario:{" "}
+         <span className=" font-normal normal-case">Jose Ordaz</span>
+     </p> 
+     <p className="font-bold mb-3 text-gray-700 uppercase">Email:{" "}
+         <span className=" font-normal normal-case">joseordazsuay@gmail.com</span>
+     </p> 
+     <p className="font-bold mb-3 text-gray-700 uppercase">Fecha Alta:{" "}
+         <span className=" font-normal normal-case">12 junio de 2023</span>
+     </p> 
+     <p className="font-bold mb-3 text-gray-700 uppercase">Sintomas:{" "}
+         <span className=" font-normal normal-case">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis repellat dolores eveniet. Quasi, vero? Blanditiis saepe doloremque autem similique! Aperiam cum asperiores nihil, debitis ipsam ea maxime at iste architecto.</span>
+     </p> 
+  
+ </div>
+  )
+}
+
+export default Paciente
